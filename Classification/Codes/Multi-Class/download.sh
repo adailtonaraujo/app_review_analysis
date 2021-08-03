e6de46b00a6c0d55d6f02e2e96af9aaa42784450
GoloMarcos <marcosgolo@usp.br>
2021-08-03 19:28:27 -0300

    organize results

--- Classification/Codes/Multi-Class/download.sh
+++ Classification/Codes/Multi-Class/download.sh
@@ -1,2 +1,4 @@
 #download ARE
-[ ! -f "../../Dataset/ARE.plk" ] && echo "Downloading ARE.plk" && gdown --id 1ctf9TKwFLEamYxSSJ97nnqm5EMh-SEdV -O "../../Dataset/ARE.plk"
+[ ! -f "../../Dataset/ARE.plk" ] && echo "Downloading ARE.plk" && gdown --id link_to_ARE -O "../../Dataset/ARE.plk"
+
+# These datasets are private and may only be used for academic purposes. We got the link to the datasets by contacting their owners. So, if you want the link to the datasets, please contact them.
